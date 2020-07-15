5b48b51e797b9c6c00b2fbd7a85881a50c77acb9
Максим <30493170+Datskovsky@users.noreply.github.com>
2020-07-15 12:30:26 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,78 @@
-/*
- * Created by Artyom Manchenkov
- * artyom@manchenkoff.me
- * manchenkoff.me © 2019
- */
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const tel = document.getElementById('tel');
+const email = document.getElementById('email');
+const checkbox = document.getElementById('checkbox');
 
-console.log('Webpack bundle loaded!');
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    checkInputs();
+});
+
+// функция проверки инпута на введенное значение
+function checkInputs(){
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const telValue = tel.value.trim();
+    const emailValue = email.value.trim();
+    const checkboxValue = checkbox.value;
+
+    // проверка заполнено ли поле username
+    if(usernameValue === ''){
+        setErrorFor(username, 'Поле "Имя" не заполнено');
+    }else{
+        setSuccessFor(username);
+    }
+
+    // проверка заполнено ли поле tel
+    if(telValue === ''){
+        setErrorFor(tel, 'Поле "Телефон" не заполнено');
+    }else if(!isTel(telValue)){
+        setErrorFor(tel, 'Поле должно иметь вид 8 (999) 123-45-64')
+    }else{
+        setSuccessFor(tel);
+    }
+
+    // проверка заполнено ли поле email
+    if(emailValue === ''){
+        setErrorFor(email, 'Поле "email" не заполнено');
+    }else if(!isEmail(emailValue)){
+        setErrorFor(email, 'Некорректный email');
+    }else{
+        setSuccessFor(email);
+    }
+
+    // проверка на свойство checked
+    if(checkbox.checked){
+        setSuccessFor(checkbox);
+    }else{
+        setErrorFor(checkbox, 'Вы должны согласиться с политикой конфедециальности');
+    }
+}
+
+// функция вывода ошибки
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+// функция одобрения
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success';
+}
+
+// проверка на валидность телефонного номера
+function isTel(tel){
+    return /^[\d]{1}\ \([\d]{2,3}\)\ [\d]{2,3}-[\d]{2,3}-[\d]{2,3}$/.test(tel);
+}
+
+// проверка на валидность email адреса
+function isEmail(email){
+    return /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/.test(email);
+}
